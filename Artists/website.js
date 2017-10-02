3b7d688ea04f8366320bf875f8be4b873d624cdd
Nicholas Mangold <nicholasmangold@gmail.com>
2017-10-02 04:25:39 -0400

    ok

---
+++ Artists/website.js
@@ -0,0 +1,17 @@
+
+function getwords(){
+    text = words.value;
+    document.getElementById("paragraph").innerHTML += '<p>'+text
+    document.getElementById("words").value = ""
+}
+
+  
+  
+$('#img1').click(function(){
+    $('.img1').fadeToggle('slow');
+});
+
+
+$('#img1').click(function(){
+    alert("Oh No! Where did Will go??");
+});
